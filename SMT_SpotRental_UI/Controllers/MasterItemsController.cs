e15eca40ee264adac4b28d37c343522f92ade2f7
VINODKUMAR\Vinod Kumar <vinodkus@gmail.com>
2017-10-26 15:19:42 +0530

    commit 33

--- SMT_SpotRental_UI/Controllers/MasterItemsController.cs
+++ SMT_SpotRental_UI/Controllers/MasterItemsController.cs
@@ -17,7 +17,7 @@ namespace SMT.SpotRental.UI.Controllers
     {
         WebAPICommunicator webAPI = null;
         
-
+        // user list
         #region MANAGE VENDORS
         public ActionResult ViewVendors()
         {
